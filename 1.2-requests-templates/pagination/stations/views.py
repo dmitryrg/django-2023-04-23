852fdad837b9e222baf3476866be669e3117880a
droganov <droganov@gontardcie.com>
2023-05-03 16:11:23 +0600

    roganov hw-3/1 import work

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -11,15 +11,25 @@ def index(request):
 def bus_stations(request):
     page_number = int(request.GET.get('page', 1))
     PER_PAGE = 10
-    stations = []
+    # stations = []
     with open('data-398-2018-08-30.csv', encoding='utf-8') as csvfile:
+        # без фильтрации
+        # stations = list(csv.DictReader(csvfile))
+
+        # с фильтрацией
         reader = csv.DictReader(csvfile)
-        for row in reader:
-            stations.append({
-                'Name': row['Name'],
-                'Street': row['Street'],
-                'District': row['District']
-            })
+        stations = [{'Name': row['Name'], 'Street': row['Street'], 'District': row['District']} for row in reader]
+        # так не работает, хотя видел в лекции пример через точку
+        # stations = [{'Name': row.Name, 'Street': row.Street, 'District': row.District} for row in reader]
+
+        # было с фильтрацией
+        # reader = list(csv.DictReader(csvfile))
+        # for row in reader:
+        #     stations.append({
+        #         'Name': row['Name'],
+        #         'Street': row['Street'],
+        #         'District': row['District']
+        #     })
 
     paginator = Paginator(stations, PER_PAGE)
     page = paginator.get_page(page_number)
--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=200, null=False)
+    price = models.DecimalField(decimal_places=0, max_digits=6)
+    image = models.TextField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(max_length=200)
