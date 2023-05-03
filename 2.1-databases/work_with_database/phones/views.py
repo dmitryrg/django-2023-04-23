25f9ecc2dc3540e2cc55c97332aadae5e522a740
droganov <droganov@gontardcie.com>
2023-05-03 17:05:52 +0600

    roganov hw-3/1 sort ready

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
 
 def index(request):
     return redirect('catalog')
 
 
 def show_catalog(request):
+    sort = request.GET.get('sort')
     template = 'catalog.html'
-    context = {}
+    _phones = Phone.objects.all()
+    phones = [
+        {'name': phone.name, 'price': phone.price, 'image': phone.image, 'slug': phone.slug}
+        for phone in _phones
+    ]
+
+    def sort_func():
+        if sort == 'name':
+            return lambda x: x['name']
+        if sort == 'min_price':
+            return lambda x: x['price']
+        if sort == 'max_price':
+            return lambda x: -x['price']
+    # лучше сортировать запросом к базе, но пока не понятно как
+    context = {'phones': sorted(phones, key=sort_func())}
     return render(request, template, context)
 
 
