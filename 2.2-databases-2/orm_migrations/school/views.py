8e1b53878e96356809328b10d2598d4273f8b891
droganov <droganov@gontardcie.com>
2023-05-11 13:19:53 +0600

    roganov hw-4/1 school - ini ver work

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -1,12 +1,31 @@
-from django.views.generic import ListView
+# from django.views.generic import ListView
 from django.shortcuts import render
 
-from .models import Student
+from school.models import Teacher, Student
 
 
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
+
+    students = Student.objects.select_related('teacher').all()
+#     { %
+#     for student in object_list %}
+#     < li > {{student.name}}
+#     {{student.group}} < br > Преподаватель: {{student.teacher.name}}
+#     {{student.teacher.subject}} < / li >
+# { % endfor %}
+    object_list = []
+    for student in students:
+        object_list.append({
+            'name': student.name,
+            'group': student.group,
+            'teacher': {
+                'name': student.teacher.name,
+                'subject': student.teacher.subject
+            }
+        })
+
+    context = {'object_list': object_list}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -75,7 +75,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_orm_migrations',
         'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PORT': '5434',
+        'USER': 'postgres',
+        'PASSWORD': '12',
     }
 }
 
@@ -125,6 +127,8 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 try:
     from .settings_local import *
