42e245f60ae646cb6ae25384ee79151551c27c25
droganov <droganov@gontardcie.com>
2023-05-11 14:44:48 +0600

    roganov hw-4/1 school - ready

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -7,28 +7,8 @@ from school.models import Teacher, Student
 def students_list(request):
     template = 'school/students_list.html'
 
-    students = Student.objects.select_related('teacher').all()
-#     { %
-#     for student in object_list %}
-#     < li > {{student.name}}
-#     {{student.group}} < br > Преподаватель: {{student.teacher.name}}
-#     {{student.teacher.subject}} < / li >
-# { % endfor %}
-    object_list = []
-    for student in students:
-        object_list.append({
-            'name': student.name,
-            'group': student.group,
-            'teacher': {
-                'name': student.teacher.name,
-                'subject': student.teacher.subject
-            }
-        })
+    students = Student.objects.prefetch_related('teachers').all().order_by('group')
 
-    context = {'object_list': object_list}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
+    context = {'object_list': students}
 
     return render(request, template, context)
