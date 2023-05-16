d4d699e68f155d1ba19db7f97a80655c97e58f75
droganov <droganov@gontardcie.com>
2023-05-16 12:54:43 +0600

    roganov hw-4/2 articles - prepare

--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -6,7 +6,7 @@ from articles.models import Article
 def articles_list(request):
     template = 'articles/news.html'
 
-    articles = Article.object.all().order_by('-published_at')
+    articles = Article.objects.all().order_by('-published_at')
     context = {'object_list': articles}
 
     return render(request, template, context)
