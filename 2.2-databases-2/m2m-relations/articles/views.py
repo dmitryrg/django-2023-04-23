45b39e1bda24712f991a93120a5ee4d780a04299
droganov <droganov@gontardcie.com>
2023-05-16 13:41:58 +0600

    roganov hw-4/2 articles - prepare

--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -6,11 +6,7 @@ from articles.models import Article
 def articles_list(request):
     template = 'articles/news.html'
 
-    # articles = Article.objects.all()
     articles = Article.objects.prefetch_related('scopes').all()
-    # for article in articles:
-    #     for scope in article.scopes.all():
-    #         print(scope.is_main)
 
     context = {'object_list': articles}
 
